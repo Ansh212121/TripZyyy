ae19f23044bb403164c6a40015476a1a1240a211
Ansh212121 <ansh2109ag@gmail.com>
2025-07-25 01:31:38 +0530

    updated nest.configg

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // You can add other Next.js config options here
-  // e.g., images, redirects, rewrites, etc.
-  
-  // Note: experimental.serverActions has been removed, as Server Actions
-  // are enabled by default in Next.js 14.
-};
+  // ...other config
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
