f2eb842446a9f22805697feff419c6ae72f8a96e
Ansh212121 <ansh2109ag@gmail.com>
2025-07-25 01:00:11 +0530

    updated nest.config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
+  // You can add other Next.js config options here
+  // e.g., images, redirects, rewrites, etc.
+  
+  // Note: experimental.serverActions has been removed, as Server Actions
+  // are enabled by default in Next.js 14.
 };
 
 module.exports = nextConfig;
